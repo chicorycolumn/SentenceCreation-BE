32c40357db12b437f93be41d3e040d29ddcb0b8d
C <chrismatusemail@gmail.com>
2021-09-08 08:43:19 +0100

    Restructure folder.

---
+++ utils/manipulateRawData/manipulateRawUtils.js
@@ -0,0 +1,141 @@
+const mrUtils = require("./manipulateRawUtils.js");
+const uUtils = require("../universalUtils.js");
+const ref = require("./reference.js");
+
+exports.findInflections = (consituentWords) => {
+  let res = {};
+
+  consituentWords.forEach((cw) => {
+    let wordValue = cw.word;
+    let inflectionsString = cw.traits;
+
+    let matchFound = false;
+
+    ref.otherInflectionsRef.forEach((higherInfectionKey) => {
+      //eg "singular"
+      if (inflectionsString.toLowerCase().includes(higherInfectionKey)) {
+        matchFound = true;
+        ref.inflectionsRef.forEach((inflectionKey) => {
+          //eg "nominative"
+          if (inflectionsString.toLowerCase().includes(inflectionKey)) {
+            uUtils.addToArrayAtKey(
+              res,
+              [higherInfectionKey, inflectionKey],
+              wordValue
+            );
+          }
+        });
+      }
+    });
+
+    if (!matchFound) {
+      let higherInfectionKey = "unsorted";
+      ref.inflectionsRef.forEach((inflectionKey) => {
+        //eg "nominative"
+        if (inflectionsString.toLowerCase().includes(inflectionKey)) {
+          uUtils.addToArrayAtKey(
+            res,
+            [higherInfectionKey, inflectionKey],
+            wordValue
+          );
+        }
+      });
+    }
+  });
+
+  return res;
+};
+
+exports.makeLemmaObjectIDs = (protoLObjs, lang, existingLemmaObjects) => {
+  function makeIds(protoLObj, counts, lang, wordtypeCode) {
+    let idNum = uUtils.numToString(counts[wordtypeCode] + 1, 3);
+    let id = `${lang.toLowerCase()}-${wordtypeCode}-${idNum}-${
+      protoLObj.lemma
+    }`;
+    counts[wordtypeCode]++;
+    return id;
+  }
+
+  let counts = { nco: 0, npe: 0, ncp: 0, npp: 0 };
+  if (existingLemmaObjects) {
+    Object.keys(counts).forEach((wordtypeCode) => {
+      let lObjs = existingLemmaObjects.filter(
+        (l) => l.id.split("-")[1] === wordtypeCode
+      );
+
+      lObjs.sort(
+        (a, b) => parseInt(b.id.split("-")[2]) - parseInt(a.id.split("-")[2])
+      );
+
+      let highestIdNumber = parseInt(lObjs[0].id.split("-")[2]);
+
+      counts[wordtypeCode] = highestIdNumber;
+    });
+  } else {
+    console.log(
+      "No existing lemma objects given so couldn't take into account existing numbers already used when making lemma object IDs."
+    );
+  }
+
+  protoLObjs.forEach((protoLObj) => {
+    let wordtypeCode = protoLObj.isPerson ? "npe" : "nco";
+    protoLObj.id = makeIds(protoLObj, counts, lang, wordtypeCode);
+  });
+
+  let headWordsThatHaveMultipleProtoLObjs = [];
+  let allHeadWords = protoLObjs.map((p) => p.lemma);
+  let tempArr = [];
+  allHeadWords.forEach((h) => {
+    if (
+      tempArr.includes(h) &&
+      !headWordsThatHaveMultipleProtoLObjs.includes(h)
+    ) {
+      headWordsThatHaveMultipleProtoLObjs.push(h);
+    }
+    tempArr.push(h);
+  });
+
+  headWordsThatHaveMultipleProtoLObjs.forEach((h) => {
+    protoLObjs.forEach((p) => {
+      if (p.lemma === h) {
+        p.id = `${p.id}-(${p.gender})`;
+        console.log(`Added (gender) to this id as it's a multiple: "${p.id}"`);
+      }
+    });
+  });
+};
+
+exports.reorderProtoLObjs = (protoLObjs) => {
+  let { pop, unpop } = mrUtils.sortProtoLObjsByPopulated(protoLObjs);
+  return [...pop, ...unpop];
+};
+
+exports.splitAllStrings = (arr, separator = " ") => {
+  let res = [];
+  arr.forEach((s) => {
+    res.push(...s.split(separator));
+  });
+  return res;
+};
+
+exports.sortProtoLObjsByPopulated = (arr) => {
+  let pop = arr.filter((p) => p.constituentWords.length);
+  let unpop = arr.filter((p) => !p.constituentWords.length);
+  return { pop, unpop };
+};
+
+// let data = JSON.stringify(getHeadWords(nouns));
+// fs.writeFile(path, data, (err) => {
+//   if (err) console.log(err);
+//   else {
+//     console.log("File written successfully\n");
+//   }
+// });
+//
+// function getHeadWords(raw) {
+//   let headWords = raw
+//     .filter((rawObj) => rawObj.senses.some((sense) => !sense.form_of))
+//     .map((rawObj) => rawObj.word);
+
+//   return Array.from(new Set(headWords));
+// }
---
+++ utils/manipulateRawData/reference.js
@@ -0,0 +1,110 @@
+exports.shorthandInflectionsRef = {
+  noms: ["singular", "nominative"],
+  accs: ["singular", "accusative"],
+  dats: ["singular", "dative"],
+  gens: ["singular", "genitive"],
+  inss: ["singular", "instrumental"],
+  locs: ["singular", "locative"],
+  vocs: ["singular", "vocative"],
+  nomp: ["plural", "nominative"],
+  accp: ["plural", "accusative"],
+  datp: ["plural", "dative"],
+  genp: ["plural", "genitive"],
+  insp: ["plural", "instrumental"],
+  locp: ["plural", "locative"],
+  vocp: ["plural", "vocative"],
+};
+
+exports.mascKeys = ["m", "masculine", "masc", "male"];
+
+exports.femKeys = ["f", "feminine", "fem", "female"];
+
+exports.genderConversionRef = {
+  "m-pr": "m1",
+  "m-an": "m2",
+  "m-in": "m3",
+
+  "m-p": "virile",
+
+  m: "nonvirile",
+  p: "nonvirile",
+  nv: "nonvirile",
+  pl: "nonvirile",
+  "m-in-p": "nonvirile",
+
+  f: "f",
+  n: "n",
+};
+
+exports.inflectionsRef = [
+  "nominative",
+  "genitive",
+  "dative",
+  "accusative",
+  "instrumental",
+  "locative",
+  "vocative",
+];
+
+exports.otherInflectionsRef = [
+  "plural",
+  "singular",
+  "augmentative",
+  "diminutive",
+  "abbreviation",
+];
+
+function checkRawInfKeys() {
+  let koala = {
+    pos: "noun",
+    heads: [{ 1: "f", template_name: "pl-noun" }],
+    inflection: [{ genp: "koali", template_name: "pl-decl-noun-f" }],
+    sounds: [
+      { ipa: "/k\u0254\u02c8a.la/" },
+      { audio: "Pl-koala.ogg", text: "audio" },
+    ],
+    word: "koala",
+    lang: "Polish",
+    lang_code: "pl",
+    senses: [
+      {
+        tags: ["feminine"],
+        glosses: ["koala"],
+        categories: ["Marsupials"],
+        id: "koala-noun",
+      },
+    ],
+  };
+
+  let rawInfKeys = [];
+
+  nouns.forEach((rawObj, rIndex) => {
+    if (!goodNounsPL.includes(rawObj.word)) {
+      return;
+    }
+    rawObj.senses.forEach((sense, sIndex) => {
+      if (!sense.form_of) {
+        return;
+      }
+
+      sense.form_of.forEach((f, fIndex) => {
+        if (f.lemma) {
+          throw `Error 2763`;
+        }
+
+        let traits = splitAllStrings(sense.glosses);
+        traits = traits.filter(
+          (t) => !["of"].includes(t) && !goodNounsPL.includes(t)
+        );
+        traits.forEach((tr) => rawInfKeys.push(tr));
+      });
+    });
+  });
+
+  rawInfKeys = rawInfKeys.filter(
+    (rik) =>
+      !ref.inflectionsRef.includes(rik.toLowerCase()) &&
+      !ref.otherInflectionsRef.includes(rik.toLowerCase())
+  );
+  rawInfKeys = Array.from(new Set(rawInfKeys));
+}
