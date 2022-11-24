a08b4b56ff549c796dd0f9f3cd13fce0de3b5039
C <chrismatusemail@gmail.com>
2022-11-24 10:29:56 +0000

    Delete unnec files.

--- utils/manipulateRawData/languageSpecific/ENG/reference.js
+++
@@ -1,5 +0,0 @@
-exports.inflectionsRef = ["nominative", "genitive"];
-
-exports.higherInflectionsRef = ["plural", "singular"];
-
-exports.otherInflectionsRef = ["augmentative", "diminutive", "abbreviation"];
--- utils/manipulateRawData/languageSpecific/ENG/utils.js
+++
@@ -1,6 +0,0 @@
-const mrUtils = require("../../manipulateRawUtils.js");
-const uUtils = require("../../../universalUtils.js");
-const consol = require("../../../zerothOrder/consoleLoggingUtils.js");
-const ref = require("./reference.js");
-
-exports.formulateInflections = (protoLObj) => {};
