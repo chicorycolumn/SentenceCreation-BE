3ee83c91141ed8fbcf71abd0ebb5f45d92afb295
C <chrismatusemail@gmail.com>
2022-11-28 00:17:52 +0000

    Remove unneeded argument.

--- spec/app05.spec.js
+++ spec/app05.spec.js
@@ -180,46 +180,46 @@ describe("/api", function () {
 
   describe("/palette - Stage 24-i: Spanish basic.", () => {
     it("#pal23-01a GET 200 YES: Polspa. Red onion (NORMAL).", () => {
-      return runPaletteTest("POL", "SPA", "dummy73a", dummy73a, true);
+      return runPaletteTest("POL", "SPA", "dummy73a", dummy73a);
     });
     it("#pal23-01b GET 200 YES: Spapol. Red onion (NORMAL).", () => {
-      return runPaletteTest("SPA", "POL", "dummy73a", dummy73a, true);
+      return runPaletteTest("SPA", "POL", "dummy73a", dummy73a);
     });
     it("#pal23-01c GET 200 YES: Engspa. Red onion (NORMAL).", () => {
-      return runPaletteTest("ENG", "SPA", "dummy73a", dummy73a, true);
+      return runPaletteTest("ENG", "SPA", "dummy73a", dummy73a);
     });
     it("#pal23-01d GET 200 YES: Spaeng. Red onion (NORMAL).", () => {
-      return runPaletteTest("SPA", "ENG", "dummy73a", dummy73a, true);
+      return runPaletteTest("SPA", "ENG", "dummy73a", dummy73a);
     });
   });
 
   describe("/palette - Stage 24-ii: Spanish basic.", () => {
     it("#pal23-02a GET 200 YES: Polspa. Red doctor (MGN).", () => {
-      return runPaletteTest("POL", "SPA", "dummy73b", dummy73bSpaPolBoth, true);
+      return runPaletteTest("POL", "SPA", "dummy73b", dummy73bSpaPolBoth);
     });
     it("#pal23-02b GET 200 YES: Spapol. Red doctor (MGN).", () => {
-      return runPaletteTest("SPA", "POL", "dummy73b", dummy73bSpaPolBoth, true);
+      return runPaletteTest("SPA", "POL", "dummy73b", dummy73bSpaPolBoth);
     });
     it("#pal23-02c GET 200 YES: Engspa. Red doctor (MGN).", () => {
-      return runPaletteTest("ENG", "SPA", "dummy73b", dummy73bEngSpa, true);
+      return runPaletteTest("ENG", "SPA", "dummy73b", dummy73bEngSpa);
     });
     it("#pal23-02d GET 200 YES: Spaeng. Red doctor (MGN).", () => {
-      return runPaletteTest("SPA", "ENG", "dummy73b", dummy73bSpaEng, true);
+      return runPaletteTest("SPA", "ENG", "dummy73b", dummy73bSpaEng);
     });
   });
 
   describe("/palette - Stage 24-i: Spanish basic.", () => {
     it("#pal23-03a GET 200 YES: Polspa. Red mother (SMP).", () => {
-      return runPaletteTest("POL", "SPA", "dummy73c", dummy73cRefPolSpa, true);
+      return runPaletteTest("POL", "SPA", "dummy73c", dummy73cRefPolSpa);
     });
     it("#pal23-03b GET 200 YES: Spapol. Red mother (SMP).", () => {
-      return runPaletteTest("SPA", "POL", "dummy73c", dummy73cRefSpaPol, true);
+      return runPaletteTest("SPA", "POL", "dummy73c", dummy73cRefSpaPol);
     });
     it("#pal23-03c GET 200 YES: Engspa. Red mother (SMP).", () => {
-      return runPaletteTest("ENG", "SPA", "dummy73c", dummy73cRefEngSpa, true);
+      return runPaletteTest("ENG", "SPA", "dummy73c", dummy73cRefEngSpa);
     });
     it("#pal23-03d GET 200 YES: Spaeng. Red mother (SMP).", () => {
-      return runPaletteTest("SPA", "ENG", "dummy73c", dummy73cRefSpaEng, true);
+      return runPaletteTest("SPA", "ENG", "dummy73c", dummy73cRefSpaEng);
     });
   });
 });
