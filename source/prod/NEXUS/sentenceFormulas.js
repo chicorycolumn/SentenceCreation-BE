986c8e66181d612453c713f61869637e2d25e98f
C <chrismatusemail@gmail.com>
2024-12-26 20:40:03 +0000

    Add sentence formulas.

--- source/prod/NEXUS/sentenceFormulas.js
+++ source/prod/NEXUS/sentenceFormulas.js
@@ -15,4 +15,84 @@ exports.sentenceFormulas = [
     topics: ["basics"],
     difficulty: [],
   },
+  {
+    key: "SF-00002",
+    equivalents: {
+      ENG: ["ENG-00002"],
+      POL: ["POL-00002"],
+      SPA: [],
+    },
+    topics: ["cuisine"],
+    difficulty: [],
+  },
+  {
+    key: "SF-00003",
+    equivalents: {
+      ENG: ["ENG-00003"],
+      POL: ["POL-00003"],
+      SPA: [],
+    },
+    topics: ["cuisine"],
+    difficulty: [],
+  },
+  {
+    key: "SF-00004",
+    equivalents: {
+      ENG: ["ENG-00004"],
+      POL: ["POL-00004"],
+      SPA: [],
+    },
+    topics: ["zoology"],
+    difficulty: [],
+  },
+  {
+    key: "SF-00005",
+    equivalents: {
+      ENG: ["ENG-00005"],
+      POL: ["POL-00005"],
+      SPA: [],
+    },
+    topics: ["zoology"],
+    difficulty: [],
+  },
+  {
+    key: "SF-00006",
+    equivalents: {
+      ENG: ["ENG-00006"],
+      POL: ["POL-00006"],
+      SPA: [],
+    },
+    topics: ["small objects"],
+    difficulty: [],
+  },
+  {
+    key: "SF-00007",
+    equivalents: {
+      ENG: ["ENG-00007"],
+      POL: ["POL-00007"],
+      SPA: [],
+    },
+    topics: ["small objects"],
+    difficulty: [],
+  },
+  {
+    key: "SF-00008",
+    equivalents: {
+      ENG: ["ENG-00008"],
+      POL: ["POL-00008"],
+      SPA: [],
+    },
+    topics: ["large objects"],
+    difficulty: [],
+  },
+  {
+    key: "SF-00009",
+    equivalents: {
+      ENG: ["ENG-00009"],
+      POL: ["POL-00009"],
+      SPA: [],
+    },
+    topics: ["large objects"],
+    difficulty: [],
+  },
 ];
